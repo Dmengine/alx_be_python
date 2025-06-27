4200809882c5d76f786a6914ce2d2cad9fe80a93
Olaweb3493 <46165459+Dmengine@users.noreply.github.com>
2025-06-27 18:18:37 +0100

    bank account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,7 +1,5 @@
-
-
 class BankAccount:
-    def __init__ (self, initial_balance=0):
+    def __init__(self, initial_balance=0):
         self.account_balance = initial_balance
 
     def deposit(self, amount):
@@ -15,4 +13,4 @@ class BankAccount:
             return False
 
     def display_balance(self):
-        print(f"Current balance: ${self.account_balance}") 
+        print(f"Current Balance: ${self.account_balance}")
